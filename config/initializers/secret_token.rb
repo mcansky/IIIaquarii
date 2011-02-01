1384f72ac13c94722841cad391fe516d5047e49a
Marc Lagrange <rhaamo@gruik.at>
2011-02-01 07:13:44 +0100

    First import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IIIaquarii::Application.config.secret_token = '744519d593f2a5b778dd7855f4d86d94fc5e53c9b9e1d1f2d0e0fcd6c2303d4cb8ac1fe7cb7a1fafde259202057ded8838f6e56492711cbfb2443f9152c2d20e'
