1a0758c98eba075f549de0148467a9463835edf0
Marc Lagrange <rhaamo@gruik.at>
2011-02-01 14:11:12 +0100

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110201130335) do
+ActiveRecord::Schema.define(:version => 20110201130827) do
 
   create_table "aq_branches", :force => true do |t|
     t.string   "name"
     t.integer  "aq_repository_id"
     t.integer  "owner_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "aq_commits", :force => true do |t|
+    t.string   "sha"
+    t.text     "log"
+    t.integer  "aq_branch_id"
+    t.integer  "author_id"
+    t.string   "author_name"
+    t.datetime "committed_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
