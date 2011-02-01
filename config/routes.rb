a4427bdd9b717bf03dbd0a09dee8a8371bfacd39
Marc Lagrange <rhaamo@gruik.at>
2011-02-01 21:13:13 +0100

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,18 @@ IIIaquarii::Application.routes.draw do
   resources :ssh_keys
   resources :aq_repositories
 
+  # fork
+  match "/aq_repositories/fork/:id" => "aq_repositories#fork"
+
+  # browse
+  match "/aq_repositories/browse/:id/(:dir)", :to => "aq_repositories#show", :dir => /(.*)/
+
+  # view a file
+  match "/aq_repositories/view_file/:id/(:path)", :to => "aq_repositories#view_file", :path => /(.*)/
+
+  # commit log
+  match "/aq_repositories/commits/:id", :to => "aq_repositories#show_commits"
+  match "/aq_repositories/commits/:id/:commit_id", :to => "aq_repositories#show_commit"
+
   root :to => "application#index"
 end
