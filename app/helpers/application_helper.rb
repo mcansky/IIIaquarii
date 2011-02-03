b34033264ca52ece736996a217b74660ce86ab31
Marc Lagrange <rhaamo@gruik.at>
2011-02-03 09:45:26 +0100

    Tweaks for the view_file template, best matching for mime types

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def is_text? fname
+    if fname =~ /^(text)\/(.*)$/ or
+        fname =~ /^(application)\/(javascript|xhtml\+xml|json|xml|ruby|x-ruby|python|x-python)$/
+      return true
+    else
+      return false
+    end
+  end
+
+  def is_image? fname
+    if fname =~ /^(image)\/(gif|jpeg|png|svg\+xml)$/
+      return true
+    else
+      false
+    end
+  end
 end
