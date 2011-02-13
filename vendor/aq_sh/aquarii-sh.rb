0da140042e7e143277852b16ff05079ed2a28f4f
Marc Lagrange <rhaamo@gruik.at>
2011-02-13 16:15:27 +0100

    Improve a bit the error handling stuff in aquarii shell

--- vendor/aq_sh/aquarii-sh.rb
+++ vendor/aq_sh/aquarii-sh.rb
@@ -1,12 +1,21 @@
 #!/usr/bin/env ruby
 
-require 'pathname'
+begin
+  require 'pathname'
+  base = Pathname(__FILE__).expand_path
+  lib = base + "../lib/aq_lib.rb"
 
-base = Pathname(__FILE__).expand_path
-lib = base + "../lib/aq_lib.rb"
+  require lib
 
-require lib
-
-# kick start the thing :
-AqLib::Command.kickstart!(ARGV[0], ENV["SSH_ORIGINAL_COMMAND"])
-#AqLib::Command.kickstart!("marc@ananas.lan.sigpipe.me", "test")
+  # kick start the thing :
+  AqLib::Command.kickstart!(ARGV[0], ENV["SSH_ORIGINAL_COMMAND"])
+  #AqLib::Command.kickstart!("marc@ananas.lan.sigpipe.me", "test")
+rescue => e
+  STDERR.puts "REMOTE: IIIaquarii: An error occured, please contact administrator."
+  a=File.open(File.expand_path("../../../errors_aq_sh", __FILE__), "a")
+  a << Time.now
+  a << " - "
+  a << e
+  a << "\n"
+  a.close
+end
