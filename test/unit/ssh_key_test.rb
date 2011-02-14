2a538676dc12b5e07d55ce6f0aa7c057bd2249f8
Marc Lagrange <rhaamo@gruik.at>
2011-02-14 02:28:24 +0100

    Add tests for SshKey

--- test/unit/ssh_key_test.rb
+++ test/unit/ssh_key_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class SshKeyTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
+  should belong_to(:user)
+  should validate_presence_of(:name)
+  should ensure_length_of(:name).is_at_most(50)
+  should validate_presence_of(:key)
 end
